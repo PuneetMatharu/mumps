070390bd31ab3779401c522063bf60186b1e5d1b
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-10-18 18:53:37 +0100

    Patch install.cmake.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,32 +2,32 @@
 
 include(CMakePackageConfigHelpers)
 
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/config.cmake.in
-${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
-INSTALL_DESTINATION cmake
-)
+set(MUMPS_INSTALL_CONFIG_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
+
+configure_package_config_file(
+  ${CMAKE_CURRENT_LIST_DIR}/config.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  INSTALL_DESTINATION ${MUMPS_INSTALL_CONFIG_DIR})
 
 write_basic_package_version_file(
-${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
-COMPATIBILITY SameMajorVersion
-)
+  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  COMPATIBILITY SameMajorVersion)
 
-install(EXPORT ${PROJECT_NAME}-targets
-NAMESPACE ${PROJECT_NAME}::
-DESTINATION cmake
-)
+install(
+  EXPORT ${PROJECT_NAME}-targets
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION ${MUMPS_INSTALL_CONFIG_DIR})
 
-install(FILES
-${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
-${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
-DESTINATION cmake
-)
+install(
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  DESTINATION ${MUMPS_INSTALL_CONFIG_DIR})
 
 # allow use of package from build directory without installing
-export(EXPORT ${PROJECT_NAME}-targets
-FILE ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}-targets.cmake
-NAMESPACE ${PROJECT_NAME}::
-)
+export(
+  EXPORT ${PROJECT_NAME}-targets
+  FILE ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}-targets.cmake
+  NAMESPACE ${PROJECT_NAME}::)
 
 # --- CPack
 
@@ -38,12 +38,15 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/package)
 
 # not .gitignore as its regex syntax is more advanced than CMake
-set(CPACK_SOURCE_IGNORE_FILES .git/ .github/ .vscode/ _CPack_Packages/
-${CMAKE_BINARY_DIR}/ ${PROJECT_BINARY_DIR}/
-)
+set(CPACK_SOURCE_IGNORE_FILES
+    .git/
+    .github/
+    .vscode/
+    _CPack_Packages/
+    ${CMAKE_BINARY_DIR}/
+    ${PROJECT_BINARY_DIR}/)
 
 install(FILES ${CPACK_RESOURCE_FILE_README} ${CPACK_RESOURCE_FILE_LICENSE}
-DESTINATION share/docs/${PROJECT_NAME}
-)
+        DESTINATION share/docs/${PROJECT_NAME})
 
 include(CPack)
